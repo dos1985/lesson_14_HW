e197edcacaa7f3461723bdd776358452acd392c6
User <dossovmarlen85@gmail.com>
2022-10-17 13:39:23 +0300

    added @app.get("/movie/<title>") def search_by_title_view(title):

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import json
 import sqlite3
-import flask
+from flask import Flask
+
+app = Flask(__name__)
+
 
 def get_value_from_db(sql):
     with sqlite3.connect("netflix.db") as connection:
@@ -10,10 +14,26 @@ def get_value_from_db(sql):
 
 def search_by_title(title):
     sql = f"""select * 
-             from netflix
-             where title = '{title}'
-             and release_year =
-             """
+            from netflix
+            where title = '#Roxy'
+            order by release_year desc
+            
+    """
     result = get_value_from_db(sql)
     for item in result:
-        return dict(result)
+        return dict(item)
+
+
+@app.get("/movie/<title>")
+def search_by_title_view(title):
+    result = search_by_title(title=title)
+    return app.response_class(
+        response=json.dump(result, ensure_ascii=False, indent=4),
+        status=200,
+        mimetype="application/json"
+
+    )
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
